63aefcc79d2a39186171aaece0264c47addef67e
thomga <gabriel.thom02@gmail.com>
2024-04-22 16:14:35 -0300

    hope now it works

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -4,7 +4,7 @@ import JSBI from 'jsbi'
 import { pack, keccak256 } from '@ethersproject/solidity'
 import { getCreate2Address } from '@ethersproject/address'
 
-import { FACTORY_ADDRESS, FACTORY_ADDRESS_MAP, INIT_CODE_HASH, MINIMUM_LIQUIDITY, FIVE, _997, _1000, ONE, ZERO } from '../constants'
+import { FACTORY_ADDRESS, FACTORY_ADDRESS_MAP, INIT_CODE_HASH_MAP, INIT_CODE_HASH, MINIMUM_LIQUIDITY, FIVE, _997, _1000, ONE, ZERO } from '../constants'
 import { InsufficientReservesError, InsufficientInputAmountError } from '../errors'
 
 export const computePairAddress = ({
@@ -17,10 +17,11 @@ export const computePairAddress = ({
   tokenB: Token
 }): string => {
   const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
+  const codehash = INIT_CODE_HASH_MAP[tokenA.chainId] ?? INIT_CODE_HASH
   return getCreate2Address(
     factoryAddress,
     keccak256(['bytes'], [pack(['address', 'address'], [token0.address, token1.address])]),
-    INIT_CODE_HASH
+    codehash
   )
 }
 export class Pair {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { FACTORY_ADDRESS_MAP, INIT_CODE_HASH, MINIMUM_LIQUIDITY } from './constants'
+export { FACTORY_ADDRESS_MAP, INIT_CODE_HASH, INIT_CODE_HASH_MAP, MINIMUM_LIQUIDITY } from './constants'
 
 export * from './errors'
 export * from './entities'
